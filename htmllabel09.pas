d3afc4e476432a901d28605c3d5c9a259247670d
Jan Schirrmacher <jschirrmacher@generalacoustics.com>
2024-01-11 12:02:55 +0100

    Package files renamed

--- htmllabel09.pas
+++ htmllabel09.pas
@@ -2,7 +2,7 @@
   This source is only used to compile and install the package.
  }
 
-unit Html;
+unit HtmlLabel09;
 
 {$warn 5023 off : no warning about unused units}
 interface
@@ -18,5 +18,5 @@ begin
 end;
 
 initialization
-  RegisterPackage('Html', @Register);
+  RegisterPackage('HtmlLabel09', @Register);
 end.
